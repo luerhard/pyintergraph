5ad107a41dad6312d15c23eaa98cacaace65ce0a
Lukas Erhard <luerhard@googlemail.com>
2018-10-12 10:33:23 +0200

    adding the safe_load context managers

---
+++ pyintergraph/safe_load.py
@@ -0,0 +1,32 @@
+from contextlib import contextmanager
+import sys
+
+@contextmanager
+def preserve_dlopenflags(flags):
+    """A context manager that temporarily sets the dlopen flags and then returns them to previous values.
+    """
+    outer_flags = sys.getdlopenflags()
+    try:
+        sys.setdlopenflags(flags)
+        yield
+    finally:
+        sys.setdlopenflags(outer_flags)
+
+
+@contextmanager
+def boost_python_dlopen_flags():
+    """A context manager that temporarily sets the dlopen flags for loading multiple
+    boost.python modules and then returns them to previous values.
+    """
+    flags = None
+    try:
+        import DLFCN
+        flags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
+    except:
+        import warnings
+        warnings.warn('Do not know which dlopen flags to set.')
+    if None != flags:
+        with preserve_dlopenflags(flags):
+            yield
+    else:
+        yield
